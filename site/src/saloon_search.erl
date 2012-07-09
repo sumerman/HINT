9a5e16647a4403f1738f1e32c8c61c9627be97a7
Dmitrii 'Mamut' Dimandt <dmitrii@dmitriid.com>
2012-07-09 03:55:19 +0300

    - instead of / in links

--- site/src/saloon_search.erl
+++ site/src/saloon_search.erl
@@ -60,4 +60,4 @@ function_name({M, F, A}) ->
 	             , ":", atom_to_list(F), "/", integer_to_list(A)])).
 function_name_link({M, F, A}) ->
 	lists:flatten(lists:append([atom_to_list(M)
-	             , ".html#", atom_to_list(F), "/", integer_to_list(A)])).
+	             , ".html#", atom_to_list(F), "-", integer_to_list(A)])).
