e6d43a659b4a4fa9ffd269577e4f2c92d865f8b5
Meleshkin Valery <nekro.grave@gmail.com>
2012-07-17 10:30:54 +0400

    New search worker implemented.

--- apps/hint_search/src/hs_search_worker.erl
+++ apps/hint_search/src/hs_search_worker.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %%% @author Meleshkin Valery
-%%% @copyright 2012 T-Platforms
+%%% @copyright 2012 HINT Spawnfest Team
 %%%-------------------------------------------------------------------
 
 -module(hs_search_worker).
@@ -11,7 +11,7 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0, q/2]).
+-export([start_link/1, start_link/2, stop/1, q/2]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -20,7 +20,6 @@
 -export([init/1, handle_call/3, handle_cast/2, 
          handle_info/2, terminate/2, code_change/3]).
 
--type entry()      :: {mfa(), number(), proplists:proplist()}.
 -type stage_desc() :: {module(), proplists:proplist(), term()}.
 -record(state, { 
     stages = [] :: [stage_desc()], 
@@ -31,9 +30,20 @@
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
-start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(Stages) ->
+  DefPath = dialyzer_plt:get_default_plt(),
+  PLT = dialyzer_plt:from_file(DefPath),
+  start_link(PLT, Stages).
 
+start_link(PLT, Stages) ->
+  gen_server:start_link(?MODULE, [{stages, Stages}, 
+                                  {plt, PLT}], []).
+
+stop(Wrk) ->
+  gen_server:call(Wrk, stop).
+
+-spec q(pid()|atom(), hint_search_req:req()) -> 
+  {ok, [hs_stage:entry()]}.
 q(Wrk, Req) ->
   gen_server:call(Wrk, {q, Req}).
 
@@ -42,7 +52,16 @@ q(Wrk, Req) ->
 %% ------------------------------------------------------------------
 
 init(Args) ->
-  {ok, Args}.
+  RawStages = proplists:get_value(stages, Args, []),
+  Stages = uniform_stages(RawStages),
+  PLT = proplists:get_value(plt, Args),
+  {ok, #state{
+      stages = Stages,
+      plt = PLT
+      }}.
+
+handle_call(stop, _From, State) ->
+  {stop, normal, ok, State};
 
 handle_call({q, Req}, _From, State) ->
   State1 = prepare_for_req(State, Req),
@@ -65,21 +84,28 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
-empy_entry_for_mfa(MFA) ->
-  {MFA, 0, []}.
+uniform_stages(Stages) ->
+  UF = fun
+    ({S, O})             -> {S, O, undefined};
+    (S) when is_atom(S)  -> {S, [], undefined}; 
+    (T) when is_tuple(T) -> T
+  end,
+  lists:map(UF, Stages).
+
+entry_from_sig({MFA, _Ret, _Args}) ->
+  hs_stage:empty_entry_for_mfa(MFA).
 
 process(#state{ plt=PLT } = State) ->
   Modules = sets:to_list(dialyzer_plt:all_modules(PLT)),
   Fun = fun(Mod) ->
       {value, Sigs} = dialyzer_plt:lookup_module(PLT, Mod),
-      process_entries(State, [empy_entry_for_mfa(MFA)  
-                              || {MFA, _, _} <- Sigs])
+      process_entries(State, Sigs) 
   end,
   lists:reverse(lists:keysort(2, lists:flatmap(Fun, Modules))).
 
 process_entries(State, Entries) ->
   lists:foldl(fun(Entry, Res) ->
-        case process_entry(State, Entry) of
+        case process_entry_sig(State, Entry) of
           % When Entry is 'ignore', 'skip', etc.
           Ent when is_atom(Ent) ->
             Res;
@@ -88,10 +114,11 @@ process_entries(State, Entries) ->
         end
     end, [], Entries).
 
-process_entry(#state{ stages=SL }, Entry) ->
+process_entry_sig(#state{ stages=SL }, Sig) ->
+  Entry = entry_from_sig(Sig),
   lists:foldl(fun apply_stage/2, Entry, SL).
 
--spec apply_stage(stage_desc(), entry()) -> entry().
+-spec apply_stage(stage_desc(), hs_stage:entry()) -> hs_stage:entry().
 apply_stage(_Stage, Entry) when is_atom(Entry) -> 
   % When Entry is 'ignore', 'skip', etc.
   Entry;
@@ -105,23 +132,76 @@ apply_stage({Mod, _Opt, State}, Entry) ->
       Entry 
   end.
 
-prepare_for_req(#state{ stages=SL, plt=PLT }, Req) ->
+prepare_for_req(#state{ stages=SL, plt=PLT } = State, Req) ->
   NewSL = lists:map(fun(StageDesc) ->
         prepare_stage(StageDesc, PLT, Req)
     end, SL),
-  #state{ stages=NewSL }.
+  State#state{ stages=NewSL }.
 
 -spec prepare_stage(stage_desc(), dialyzer_plt:plt(), 
                     hint_search_req:req()) -> stage_desc().
 prepare_stage({Mod, Opt, _OldState}, PLT, Req) ->
   try 
-    NewState = Mod:prepare(Opt, PLT, Req),
+    {ok, NewState} = Mod:prepare(Opt, PLT, Req),
     {Mod, Opt, NewState}
   catch 
     _:Reason -> 
       error_logger:warning_msg(
         "Stage ~p failed to prepare for request ~p. Reason: ~p",
         [Mod, Req, Reason]),
-      undefined 
+      {undefined, [], undefined}
   end.
+
+
+%%
+%% Tests
+%%
+-include_lib("eunit/include/eunit.hrl").
+-ifdef(TEST).
+
+request() ->
+  "foo:bar([A], fun((A)->B)) -> [B]".
+
+stages() ->
+  [hs_stage_arity, {hs_stage_types, []}].
+
+setup() ->
+  %% TODO don't use default PLT for this test
+  {ok, Wrk} = start_link(stages()),
+  Wrk.
+
+cleanup(Wrk) ->
+  stop(Wrk).
+
+get_first(0, _)  -> [];
+get_first(_, []) -> [];
+get_first(N, [E | List]) -> 
+  [E | get_first(N-1, List)].
+
+do_search(Wrk) ->
+  Req = hint_search_req:new(request()),
+  {ok, Res} = q(Wrk, Req),
+  ?debugFmt("Search result is: ~p~n", [get_first(5, Res)]),
+  Res.
+
+is_valid_search_reuslt([]) -> true;
+is_valid_search_reuslt([{MFA, Score1, _Ex}, 
+                        {_, Score2, _} | Rest]) 
+    when (Score1 >= Score2) and (tuple_size(MFA) == 3) -> 
+  is_valid_search_reuslt(Rest);
+is_valid_search_reuslt([{MFA, _Score, _Ex} | Rest]) 
+    when (tuple_size(MFA) == 3) -> 
+  is_valid_search_reuslt(Rest);
+is_valid_search_reuslt(_) -> false.
+
+smth_found_and_well_formated(Wrk) ->
+  [?_assert(is_process_alive(Wrk)),
+   ?_assert(is_valid_search_reuslt(do_search(Wrk)))].
+
+well_formatted_result_test_() ->
+  {setup,
+   fun setup/0, fun cleanup/1,
+   fun smth_found_and_well_formated/1}.
+
+-endif.
 
