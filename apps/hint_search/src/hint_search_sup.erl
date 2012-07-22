40deccd2d09d8ca20a2ef0b2e222b07dcc0b76a3
Meleshkin Valery <nekro.grave@gmail.com>
2012-07-23 00:25:58 +0400

    Switched controlflow to the new infrastructure

--- apps/hint_search/src/hint_search_sup.erl
+++ apps/hint_search/src/hint_search_sup.erl
@@ -24,34 +24,8 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-  NumberOfWorkers = worker_number(),
-  Workers = [plt_cache_server],
-  ChildSpecs =
-               lists:map(fun(Worker) -> pool_spec(Worker, NumberOfWorkers) end, Workers),
-  SearchSup = {hint_query_sup, {hint_query_sup, start_link, []}, permanent, infinity, supervisor, [hint_query_sup]},
-  WrkSup = ?CHILD(hs_search_api, supervisor),
-  {ok, { {one_for_one, 5, 10}, [WrkSup, SearchSup | ChildSpecs]} }.
-
-pool_spec(Worker, Number) ->
-  PoolName = list_to_atom("pool_" ++ atom_to_list(Worker)),
-  Args = [  {name, {local, PoolName}}
-          , {worker_module, Worker}
-          , {size, Number}
-          , {max_overflow, Number * 2}
-         ],
-  poolboy:child_spec(PoolName, Args).
-
-worker_number() ->
-  case  erlang:system_info(logical_processors_available) of
-    unknown ->
-      case erlang:system_info(logical_processors_online) of
-        unknown ->
-          case erlang:system_info(logical_processors) of
-            unknown -> 1;
-            LP      -> LP
-          end;
-        LPO     -> LPO
-      end;
-    LPA     -> LPA
-  end.
+  {ok, {{one_for_one, 5, 10}, [
+        ?CHILD(hint_query_sup, supervisor),
+        ?CHILD(hs_search_api, supervisor)
+        ]}}.
 
