8f01b75d8b414af93a6d0dbf1cc82959835d285b
Meleshkin Valery <nekro.grave@gmail.com>
2012-07-23 00:25:58 +0400

    Switched controlflow to the new infrastructure

--- apps/hint_search/src/hint_query_worker.erl
+++ apps/hint_search/src/hint_query_worker.erl
@@ -16,13 +16,13 @@
         ]).
 
 q(Request) ->
-	{ok, Pid} = supervisor:start_child(hint_query_sup, []),
-	Result = gen_server:call(Pid, {q, Request}, infinity),
-	supervisor:terminate_child(hint_query_sup, Pid),
-	Result.
+  {ok, Pid} = supervisor:start_child(hint_query_sup, []),
+  Result = gen_server:call(Pid, {q, Request}, infinity),
+  supervisor:terminate_child(hint_query_sup, Pid),
+  Result.
 
 start_link() ->
-	gen_server:start_link(?MODULE, [], []).
+  gen_server:start_link(?MODULE, [], []).
 
 %%
 %% gen_server callbacks
@@ -32,21 +32,21 @@ init(_Args) ->
     {ok, []}.
 
 handle_call({q, Req}, _From, State) ->
-	R = try hs_engine_types:q(Req) of
-			L -> {ok, L}
-		catch 
-			_:Reason -> {error, Reason}
-		end,
-    {reply, R, State}.
+  R = try hs_search_api:q(hint_search_req:new(Req)) of
+    L -> {ok, L}
+  catch 
+    _:Reason -> {error, Reason}
+  end,
+  {stop, normal, R, State}.
 
 handle_cast(_, State) ->
-    {noreply, State}.
+  {noreply, State}.
 
 handle_info(_, State) ->
-    {noreply, State}.
+  {noreply, State}.
 
 code_change(_, State, _) ->
-    {ok, State}.
+  {ok, State}.
 
 terminate(shutdown, _State) ->
-    ok.
+  ok.
