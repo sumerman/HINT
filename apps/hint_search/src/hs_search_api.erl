abbb01f4edce467e19088ca68dd97bf07cd8c623
Meleshkin Valery <nekro.grave@gmail.com>
2012-07-22 23:38:48 +0400

    New search infrastructure implemented.

---
+++ apps/hint_search/src/hs_search_api.erl
@@ -0,0 +1,73 @@
+%%%-------------------------------------------------------------------
+%%% @author Meleshkin Valery
+%%% @copyright 2012 HINT Spawnfest Team
+%%%-------------------------------------------------------------------
+
+-module(hs_search_api).
+
+-export([start_link/0,
+         stop/0,
+         reload/0,
+         q/1,
+         q/3]).
+
+start_link() ->
+  hs_search_worker_sup:start_link().
+
+stop() ->
+  Wrks = hs_search_worker_sup:all_workers(),
+  lists:foreach(fun(W) ->
+        try poolboy:stop(W)
+        catch
+          error:undef -> hs_search_worker:stop(W)
+        end
+    end, Wrks).
+
+-spec q(hint_search_req:req()) ->
+  {ok, [hs_stage:entry()]}.
+q(Req) ->
+  F = fun(Wrk) ->
+      hs_search_worker:q(Wrk, Req)
+  end,
+  do_q(F).
+
+-spec q(hint_search_req:req(), pos_integer(), pos_integer()) ->
+  {ok, [hs_stage:entry()]}.
+q(Req, From, Len) when From > 0, Len > 0 ->
+  F = fun(Wrk) ->
+      hs_search_worker:q(Wrk, Req, From, Len)
+  end,
+  do_q(F).
+
+do_q(F) ->
+  Wrks = hs_search_worker_sup:all_workers(),
+  lists:merge(lists:map(
+      fun(W) ->
+          {ok, R} = try_in_pool(W, F), R
+      end, Wrks)).
+
+reload() ->
+  lists:foreach(
+    fun(W) ->
+        hs_search_worker:reload(W)
+    end, get_all_workers_from_all_pools()).
+
+get_all_workers_from_all_pools() ->
+  Wrks = hs_search_worker_sup:all_workers(),
+  try
+    lists:concat([workers_from_pool(Pool)
+                  || Pool <- Wrks])
+  catch
+    error:undef -> Wrks
+  end.
+
+workers_from_pool(Pool) ->
+  Ws = gen_fsm:sync_send_all_state_event(Pool, get_all_workers),
+  [Pid || {_Id, Pid, _Type, _Mods} <- Ws].
+
+try_in_pool(Wrk, Fun) ->
+  try poolboy:transaction(Wrk, Fun)
+  catch _:_ -> Fun(Wrk) end.
+
+
+
--- apps/hint_search/src/hs_search_worker.erl
+++ apps/hint_search/src/hs_search_worker.erl
@@ -5,28 +5,32 @@
 
 -module(hs_search_worker).
 -behaviour(gen_server).
--define(SERVER, ?MODULE).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/1, start_link/2, stop/1, q/2]).
+-export([start_link/1,
+         start_link/2,
+         stop/1,
+         reload/1,
+         q/2,
+         q/4]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
 %% ------------------------------------------------------------------
 
--export([init/1, handle_call/3, handle_cast/2, 
+-export([init/1, handle_call/3, handle_cast/2,
          handle_info/2, terminate/2, code_change/3]).
 
 -type stage_desc() :: {module(), proplists:proplist(), term()}.
--record(state, { 
-    stages  = [] :: [stage_desc()], 
-    plt_src = dialyzer_plt:get_default_plt() 
-              :: file:filename() 
+-record(state, {
+    stages  = [] :: [stage_desc()],
+    plt_src = dialyzer_plt:get_default_plt()
+              :: file:filename()
                | {module(), atom(), list()},
-    plt     = undefined :: dialyzer_plt:plt() 
+    plt     = undefined :: dialyzer_plt:plt()
     }).
 
 %% ------------------------------------------------------------------
@@ -34,7 +38,7 @@
 %% ------------------------------------------------------------------
 
 start_link(PLTSrc, Stages) ->
-  start_link(?MODULE, [{stages, Stages}, 
+  start_link(?MODULE, [{stages, Stages},
                        {plt_src, PLTSrc}]).
 
 start_link(Args) ->
@@ -43,10 +47,26 @@ start_link(Args) ->
 stop(Wrk) ->
   gen_server:call(Wrk, stop).
 
--spec q(pid()|atom(), hint_search_req:req()) -> 
+-spec q(pid()|atom(), hint_search_req:req()) ->
   {ok, [hs_stage:entry()]}.
 q(Wrk, Req) ->
-  gen_server:call(Wrk, {q, Req}).
+  q(Wrk, Req, undefined).
+
+%% @private
+-spec q(pid()|atom(), hint_search_req:req(), 
+        {pos_integer(), pos_integer()} | undefined) ->
+  {ok, [hs_stage:entry()]}.
+q(Wrk, Req, Range) ->
+  gen_server:call(Wrk, {q, Req, Range}).
+
+-spec q(pid()|atom(), hint_search_req:req(), 
+        pos_integer(), pos_integer()) ->
+  {ok, [hs_stage:entry()]}.
+q(Wrk, Req, From, Len) when From > 0, Len > 0 ->
+  q(Wrk, Req, {From, Len}).
+
+reload(Wrk) ->
+  Wrk ! load_plt.
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
@@ -57,7 +77,10 @@ init(Args) ->
   Stages = uniform_stages(RawStages),
   DefSrc = dialyzer_plt:get_default_plt(),
   PLTSrc = proplists:get_value(plt_src, Args, DefSrc),
-  self() ! load_plt,
+  reload(self()),
+  %% meta only!
+  put('**plt_src**', PLTSrc),
+  put('**stages**', Stages),
   {ok, #state{
       stages  = Stages,
       plt_src = PLTSrc
@@ -66,10 +89,10 @@ init(Args) ->
 handle_call(stop, _From, State) ->
   {stop, normal, ok, State};
 
-handle_call({q, Req}, _From, State) ->
-  State1 = prepare_for_req(State, Req),
-  R = process(State1),
-  {reply, {ok, R}, State1}.
+handle_call({q, Req, Range}, _From, State) ->
+  StateTemp = prepare_for_req(State, Req),
+  R = process(StateTemp),
+  {reply, {ok, cut(R, Range)}, State}.
 
 handle_cast(_Msg, State) ->
   {noreply, State}.
@@ -78,7 +101,7 @@ handle_info(load_plt, #state{ plt_src = {M,F,A} } = State) ->
   PLT = apply(M, F, A),
   {noreply, State#state{ plt=PLT }};
 handle_info(load_plt, #state{ plt_src = Path } = State) ->
-  PLT = dialyzer_plt:from_file(Path),
+  PLT = dialyzer_plt:from_file(plt_path(Path)),
   {noreply, State#state{ plt=PLT }};
 
 handle_info(_Info, State) ->
@@ -97,7 +120,7 @@ code_change(_OldVsn, State, _Extra) ->
 uniform_stages(Stages) ->
   UF = fun
     ({S, O})             -> {S, O, undefined};
-    (S) when is_atom(S)  -> {S, [], undefined}; 
+    (S) when is_atom(S)  -> {S, [], undefined};
     (T) when is_tuple(T) -> T
   end,
   lists:map(UF, Stages).
@@ -109,7 +132,7 @@ process(#state{ plt=PLT } = State) ->
   Modules = sets:to_list(dialyzer_plt:all_modules(PLT)),
   Fun = fun(Mod) ->
       {value, Sigs} = dialyzer_plt:lookup_module(PLT, Mod),
-      process_entries(State, Sigs) 
+      process_entries(State, Sigs)
   end,
   lists:reverse(lists:keysort(2, lists:flatmap(Fun, Modules))).
 
@@ -119,7 +142,7 @@ process_entries(State, Entries) ->
           % When Entry is 'ignore', 'skip', etc.
           Ent when is_atom(Ent) ->
             Res;
-          Ent when is_tuple(Ent) -> 
+          Ent when is_tuple(Ent) ->
             [Ent|Res]
         end
     end, [], Entries).
@@ -129,17 +152,19 @@ process_entry_sig(#state{ stages=SL }, Sig) ->
   lists:foldl(fun apply_stage/2, Entry, SL).
 
 -spec apply_stage(stage_desc(), hs_stage:entry()) -> hs_stage:entry().
-apply_stage(_Stage, Entry) when is_atom(Entry) -> 
+apply_stage(_Stage, Entry) when is_atom(Entry) ->
   % When Entry is 'ignore', 'skip', etc.
+  Entry;
+apply_stage({undefined, _Opt, undefined}, Entry) ->
   Entry;
 apply_stage({Mod, _Opt, State}, Entry) ->
   try Mod:apply(State, Entry)
-  catch 
-    _:Reason -> 
+  catch
+    _:Reason ->
       error_logger:warning_msg(
         "Stage ~p failed to process entry ~p. Reason: ~p",
         [Mod, Entry, Reason]),
-      Entry 
+      Entry
   end.
 
 prepare_for_req(#state{ stages=SL, plt=PLT } = State, Req) ->
@@ -148,19 +173,32 @@ prepare_for_req(#state{ stages=SL, plt=PLT } = State, Req) ->
     end, SL),
   State#state{ stages=NewSL }.
 
--spec prepare_stage(stage_desc(), dialyzer_plt:plt(), 
+-spec prepare_stage(stage_desc(), dialyzer_plt:plt(),
                     hint_search_req:req()) -> stage_desc().
 prepare_stage({Mod, Opt, _OldState}, PLT, Req) ->
-  try 
+  try
     {ok, NewState} = Mod:prepare(Opt, PLT, Req),
     {Mod, Opt, NewState}
-  catch 
-    _:Reason -> 
+  catch
+    _:Reason ->
       error_logger:warning_msg(
         "Stage ~p failed to prepare for request ~p. Reason: ~p",
         [Mod, Req, Reason]),
       {undefined, [], undefined}
   end.
+
+cut(L, undefined) -> L;
+cut(L, {From, Len}) ->
+  lists:sublist(L, From, Len).
+
+plt_path({priv, Path}) ->
+  case application:get_application() of
+    {ok, App} ->
+      filename:join(code:priv_dir(App), Path);
+    undefined ->
+      Path
+  end;
+plt_path(Path) -> Path.
 
 %%
 %% Tests
@@ -181,7 +219,7 @@ setup_default() ->
 
 setup_mfa() ->
   PltGen = {hs_biftab_plt, tabs_to_plt, [["../priv"]]},
-  {ok, Wrk} = start_link([{stages, stages()}, 
+  {ok, Wrk} = start_link([{stages, stages()},
                           {plt_src, PltGen}]),
   Wrk.
 
@@ -190,28 +228,37 @@ cleanup(Wrk) ->
 
 get_first(0, _)  -> [];
 get_first(_, []) -> [];
-get_first(N, [E | List]) -> 
+get_first(N, [E | List]) ->
   [E | get_first(N-1, List)].
 
-do_search(Wrk) ->
+do_search(Wrk, Range) ->
   Req = hint_search_req:new(request()),
-  {ok, Res} = q(Wrk, Req),
+  {ok, Res} = q(Wrk, Req, Range),
   ?debugFmt("~nTop results: ~p~n", [get_first(5, Res)]),
   Res.
 
 is_valid_search_reuslt([]) -> true;
-is_valid_search_reuslt([{MFA, Score1, _Ex}, 
-                        {_, Score2, _} | Rest]) 
-    when (Score1 >= Score2) and (tuple_size(MFA) == 3) -> 
+is_valid_search_reuslt([{MFA, Score1, _Ex},
+                        {_, Score2, _} | Rest])
+    when (Score1 >= Score2) and (tuple_size(MFA) == 3) ->
   is_valid_search_reuslt(Rest);
-is_valid_search_reuslt([{MFA, _Score, _Ex} | Rest]) 
-    when (tuple_size(MFA) == 3) -> 
+is_valid_search_reuslt([{MFA, _Score, _Ex} | Rest])
+    when (tuple_size(MFA) == 3) ->
   is_valid_search_reuslt(Rest);
 is_valid_search_reuslt(_) -> false.
 
 smth_found_and_well_formated(Wrk) ->
-  [?_assert(is_process_alive(Wrk)),
-   ?_assert(is_valid_search_reuslt(do_search(Wrk)))].
+  Len = 5, Start = 3,
+  Unbound = do_search(Wrk, undefined),
+  Bound   = do_search(Wrk, {Start, Len}),
+  [?_assert(is_valid_search_reuslt(Unbound)),
+   ?_assert(is_valid_search_reuslt(Bound)),
+   ?_assertEqual(length(Bound), Len),
+   ?_assert(is_sublist(Bound, Unbound))].
+
+is_sublist([X|_]=Sub, List) ->
+  Rest = lists:dropwhile(fun(Y) -> Y =/= X end, List),
+  lists:prefix(Sub, Rest).
 
 well_formatted_result_test_() ->
   {"Search result for defult PLT is a descending oredered list",
