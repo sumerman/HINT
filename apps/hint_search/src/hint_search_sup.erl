1b83f5acf6bcef92049f5a5b0dc84250df3187dc
Meleshkin Valery <nekro.grave@gmail.com>
2012-07-22 23:38:48 +0400

    New search infrastructure implemented.

--- apps/hint_search/src/hint_search_sup.erl
+++ apps/hint_search/src/hint_search_sup.erl
@@ -10,7 +10,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, infinity, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -24,33 +24,34 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-	NumberOfWorkers = worker_number(),
-	Workers = [plt_cache_server],
-	ChildSpecs = 
-		lists:map(fun(Worker) -> pool_spec(Worker, NumberOfWorkers) end, Workers),
-    SearchSup = {hint_query_sup, {hint_query_sup, start_link, []}, permanent, infinity, supervisor, [hint_query_sup]},
-    {ok, { {one_for_one, 5, 10}, [SearchSup | ChildSpecs]} }.
+  NumberOfWorkers = worker_number(),
+  Workers = [plt_cache_server],
+  ChildSpecs =
+               lists:map(fun(Worker) -> pool_spec(Worker, NumberOfWorkers) end, Workers),
+  SearchSup = {hint_query_sup, {hint_query_sup, start_link, []}, permanent, infinity, supervisor, [hint_query_sup]},
+  WrkSup = ?CHILD(hs_search_api, supervisor),
+  {ok, { {one_for_one, 5, 10}, [WrkSup, SearchSup | ChildSpecs]} }.
 
 pool_spec(Worker, Number) ->
-	PoolName = list_to_atom("pool_" ++ atom_to_list(Worker)),
-	Args = [ {name, {local, PoolName}}
-	       , {worker_module, Worker}
-	       , {size, Number}
-	       , {max_overflow, Number * 2}
-	],
-    poolboy:child_spec(PoolName, Args).
+  PoolName = list_to_atom("pool_" ++ atom_to_list(Worker)),
+  Args = [  {name, {local, PoolName}}
+          , {worker_module, Worker}
+          , {size, Number}
+          , {max_overflow, Number * 2}
+         ],
+  poolboy:child_spec(PoolName, Args).
 
 worker_number() ->
-	case  erlang:system_info(logical_processors_available) of
-		unknown ->
-			case erlang:system_info(logical_processors_online) of
-				unknown -> 
-					case erlang:system_info(logical_processors) of
-						unknown -> 1;
-						LP      -> LP
-					end;
-				LPO     -> LPO
-			end;
-		LPA     -> LPA
-	end.
-					
+  case  erlang:system_info(logical_processors_available) of
+    unknown ->
+      case erlang:system_info(logical_processors_online) of
+        unknown ->
+          case erlang:system_info(logical_processors) of
+            unknown -> 1;
+            LP      -> LP
+          end;
+        LPO     -> LPO
+      end;
+    LPA     -> LPA
+  end.
+
